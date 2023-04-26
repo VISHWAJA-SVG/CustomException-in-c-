80dc2292212bd9c447beaa5e620346913578d246
VSANJEKA <VSANJEKA@INLEN8520076178>
2023-04-26 15:18:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CustomException
+{
+    class Program
+    {
+        public static void DoSomething(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Number must be non-negative.");
+            }
+            Console.WriteLine($"The number is {number}.");
+        }
+        static void Main(string[] args)
+        {
+                try
+                {
+                    DoSomething(-1);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"An exception occurred: {ex.Message}");
+                }
+        }    
+    }    
+}
